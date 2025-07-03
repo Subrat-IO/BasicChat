ed531f30f23a463f201c1828281252ba1dfb105b
subrat sethi <subratsethi501@gmail.com>
2025-07-03 23:35:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -72,5 +72,6 @@ app.delete("/chats/:id", async (req, res) => {
 // Start server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
