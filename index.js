325bf5c33c2cb3bda6beb38e759708b08d9b404b
subrat sethi <subratsethi501@gmail.com>
2025-07-03 23:26:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -113,34 +113,7 @@ async function main() {
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
-let chats = [
-  {
-    from: "subha",
-    to: "riya",
-    message: "send the notes",
-    created_at: new Date()
-  },
-  {
-    from: "riya",
-    to: "subha",
-    message: "wait, I’ll send in 5 mins",
-    created_at: new Date()
-  },
-  {
-    from: "subha",
-    to: "riya",
-    message: "ok, no problem",
-    created_at: new Date()
-  },
-  {
-    from:"siyabn",
-    to:"rishav",
-    message:"your notes sent",
-    created_at:new Date()
-  }
-];
 
-Chat.insertMany(chats);
 app.listen(8080,()=>{
     console.log("server is listening at 8080")
 })
